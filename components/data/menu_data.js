a36ac986507f9a0195c2192e920fcd68f252e1f1
Dnnguyen <dnnguyen100@gmail.com>
2023-07-10 12:58:52 -0400

    update project structure

---
+++ components/data/menu_data.js
@@ -0,0 +1,37 @@
+export let menu = [
+  {
+    name: 'French Costini',
+    image:'./images/french-crostini.jpg',
+    totalOrderedQuantity: 5290,
+    totalRevenue: 7904,
+    price: 29,
+    ingredients: 'French, Spicy'
+  },
+  
+  {
+    name: 'Fried Egg Sandwich',
+    image: './images/fried-egg-sandwich.jpg',
+    totalOrderedQuantity: 5783,
+    totalRevenue: 8460,
+    price: 34,
+    ingredients: 'Egg, Sandwich'
+  },
+
+  {
+    name: 'Pizza Hot Chilli',
+    image: './images/pizza-hot-chili.jpg',
+    totalOrderedQuantity: 3862,
+    totalRevenue: 6483,
+    price: 17,
+    ingredients: 'Hot, Spicy'
+  },
+
+  {
+    name: 'Lemon Yogurt Parfait',
+    image: './images/lemon-yoghurt.jpg',
+    totalOrderedQuantity: 2518,
+    totalRevenue: 2789,
+    price: 28,
+    ingredients: 'Juice, Cold'
+  },
+];
---
+++ components/data/order-list_data.js
@@ -0,0 +1,56 @@
+export let orderList = [
+  {
+    id: 54565,
+    orderName: 'Fresh Crostini',
+    customerName: 'Adam Smith',
+    location: 'Gulshan',
+    deliveryTime: '10:20',
+    quantity: 5,
+    price: 34,
+    status: 'Pending'
+  },
+
+  {
+    id: 54265,
+    orderName: 'Multigrain Hot Cereal',
+    customerName: 'John Doe',
+    location: 'Baridhara',
+    deliveryTime: '3:00',
+    quantity: 4,
+    price: 87,
+    status: 'Delivered'
+  },
+
+  {
+    id: 54235,
+    orderName: 'French Fry',
+    customerName: 'Maximillian',
+    location: 'Khilgaon',
+    deliveryTime: '2:00',
+    quantity: 6,
+    price: 65,
+    status: 'Cancelled'
+  },
+
+  {
+    id: 54587,
+    orderName: 'Fried Egg Sandwich',
+    customerName: 'John Johnson',
+    location: 'Gulshan',
+    deliveryTime: '11:00',
+    quantity: 3,
+    price: 56,
+    status: 'Pending'
+  },
+
+  {
+    id: 54521,
+    orderName: 'Pizza',
+    customerName: 'Mike Hussy',
+    location: 'Banani',
+    deliveryTime: '12:00',
+    quantity: 5,
+    price: 65,
+    status: 'Pending'
+  },
+];
