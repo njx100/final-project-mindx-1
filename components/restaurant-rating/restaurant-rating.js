2e9b60652b20edbc93598d5f50d27fee6ec65818
Dnnguyen <dnnguyen100@gmail.com>
2023-07-14 14:14:33 -0400

    basic restaurant rating

--- components/restaurant-rating/restaurant-rating.js
+++ components/restaurant-rating/restaurant-rating.js
@@ -1,16 +1,26 @@
 const ctx = document.getElementById("rating-doughnut-chart");
+const graphEl = document.querySelector(".graph-tab");
+const reviewsEl = document.querySelector(".reviews-tab");
+const graphContentEl = document.querySelector(".graph-tab-content");
+const reviewsContentEl = document.querySelector(".reviews-tab-content");
 const food = 220;
 const service = 420;
 const waiting_time = 260;
 const others = 460;
 
+// calculate data
+
+let total = food + service + waiting_time + others;
+
+// render html element
+
+// chart
 new Chart(ctx, {
   type: "doughnut",
   data: {
-    // labels: ["Food", "Service", "Waiting Time", "Others"],
+    labels: ["Food:", "Service:", "Waiting Time:", "Others:"],
     datasets: [
       {
-        label: "# of Votes",
         data: [food, service, waiting_time, others],
         borderWidth: 1,
         backgroundColor: [
@@ -29,5 +39,30 @@ new Chart(ctx, {
         display: false,
       },
     },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
   },
+});
+
+// active tab
+reviewsContentEl.style.display = "none";
+graphEl.addEventListener("click", () => {
+  console.log("graph clicked");
+  graphContentEl.style.display = "block";
+  reviewsContentEl.style.display = "none";
+  graphEl.classList.remove("selected-tab");
+  reviewsEl.classList.remove("selected-tab");
+  graphEl.classList.add("selected-tab");
+});
+
+reviewsEl.addEventListener("click", () => {
+  console.log("reviews clicked");
+  graphContentEl.style.display = "none";
+  reviewsContentEl.style.display = "block";
+  graphEl.classList.remove("selected-tab");
+  reviewsEl.classList.remove("selected-tab");
+  reviewsEl.classList.add("selected-tab");
 });
---
+++ components/restaurant-rating/review-data.js
@@ -0,0 +1,29 @@
+export const reviewData = [
+  {
+    img: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80",
+    name: "Antony Jonus",
+    rating: 5,
+    review:
+      "This place is our go-to spot because the service is outstanding and the management really knows how to take care of their customers.",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1561828970-daff2303fe58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80",
+    name: "Terrence Mitchell",
+    rating: 3,
+    review:
+      "I am over the days that they cannot hire and staff our stores properly. I entered the establishment and was NOT greeted",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1552162864-987ac51d1177?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80",
+    name: "Shacobia A",
+    rating: 5,
+    review: "Yummy food 😋 😋 great staff 👍👍 I highly recommend it 👌 👌",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1545194827-db87df1649d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80",
+    name: "Karen Moe",
+    rating: 2,
+    review:
+      "Every time I come here, I wonder why I come back.They give you like one slice of each kind of meat on a small sub... I always feel like I need to stop somewhere else to fill up. ",
+  },
+];
